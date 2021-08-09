ffd0cb370b1b9825583c2c65b3fd002d7275b635
Marcos-Bazbih <marcosbaza100@gmail.com>
2021-08-09 22:47:29 +0300

    add

---
+++ main.js
@@ -0,0 +1,70 @@
+// ***************************_1_****************************
+// mission1Array = [1, 2, 3, 4, 5, 6, 7, 8]
+// function missionOne(missionOneArray){
+//         missionOneArray.reverse();
+//         return missionOneArray;
+// }
+// console.log(missionOne(mission1Array));
+// ***************************_1_****************************
+
+
+
+
+// ***************************_2_****************************
+// tenNumArray = [0,5,2,0,0,0,0,0,0,0];
+// nineNumArray = [0,3,0,0,0,0,0,0,0];
+
+// function missionTwo(missionTwoArray){
+//         if(missionTwoArray.length == 10){
+//             missionTwoArray.splice(3,0,"-")
+//             missionTwoArray.splice(7,0,"-")
+//             return missionTwoArray;
+//         }      
+//         else if(missionTwoArray.length == 9){
+//             missionTwoArray.splice(2,0,"-")
+//             missionTwoArray.splice(6,0,"-")
+//             return missionTwoArray;
+//         }
+//         else{
+//             return "no"
+//         }
+//     }
+// console.log(missionTwo(nineNumArray));
+// console.log(missionTwo(tenNumArray));
+// ***************************_2_****************************
+
+
+
+
+// ***************************_3_****************************
+
+
+// ***************************_3_****************************
+
+
+
+
+// ***************************_4_****************************
+// var fact = 1;
+// function missionFour(missionFour){
+//     for(var i=1; i<=missionFour;i++){
+//         fact= fact*i;
+//     }
+//     return fact;
+// }
+// console.log(missionFour(8));
+// ***************************_4_****************************
+
+
+
+
+// ***************************_6_****************************
+// var numArray = [5,4,8,1,7,2,6,3];
+// var second = [];
+// var min;
+// function arrays(numArray){
+//     numArray.sort();
+//     return numArray;
+// }
+// console.log(arrays(numArray));
+// ***************************_6_****************************
---
+++ mission5.js
@@ -0,0 +1,35 @@
+// ***************************_5_****************************
+// var plusSum=0;
+// function missionFivePlus(num1, num2){
+//     plusSum = num1 + num2;
+//     return plusSum;
+// }
+// console.log(missionFivePlus(50, 5));
+
+
+
+// var minusSum=0;
+// function missionFiveMinus(num1, num2){
+//     minusSum = num1 - num2;
+//     return minusSum;
+// }
+// console.log(missionFiveMinus(50, 5));
+
+
+
+// var multiSum=0;
+// function missionFiveMulti(num1, num2){
+//     multiSum = num1 * num2;
+//     return multiSum;
+// }
+// console.log(missionFiveMulti(50, 5));
+
+
+
+// var divisionSum=0;
+// function missionFiveDivision(num1, num2){
+//     divisionSum = num1 / num2;
+//     return divisionSum;
+// }
+// console.log(missionFiveDivision(50, 5));
+// ***************************_5_****************************
